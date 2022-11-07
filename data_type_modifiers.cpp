82103990b7789a87744f7f5b24672b6294b11bdb
Aryan Tiwari <at150@snu.edu.in>
2022-11-08 00:14:30 +0530

    short data type modifier

--- data_type_modifiers.cpp
+++ data_type_modifiers.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
-    //initialize variables
-    int integer = 2147483649; // this is the value greater than the upper limit of the int data type
-    //display variable value
-    cout << integer;
+int main() {
+  // Initialize variables
+  int integer = 32768;
+  short int Sinteger = 32768;
+  // Display variables value
+  cout << "integer = " << integer << endl;
+  cout << "short_integer = " << Sinteger << endl;
 }
