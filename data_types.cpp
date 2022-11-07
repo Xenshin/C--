2389d358c30bcc765af17ba092c9ef26028dcb0a
Aryan Tiwari <at150@snu.edu.in>
2022-11-07 13:54:00 +0530

    int, float, double data types

---
+++ data_types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // create variable of different types
+    int integer_number = 10;
+    float float_number = 10.5;
+    /* stores scientific value. the number after "e" represents the 
+    power of 10*/
+    float float_scientific = 97e4;
+    double double_number = 10.5;
+
+    // print values of variables
+    cout << "int = " << integer_number << endl;
+    cout << "float = " << float_number << endl;
+    cout << "float_scientific = " << float_scientific << endl;
+    cout << "double = " << double_number << endl;
+}
