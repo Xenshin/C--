81bf014b4f1f43585200b13bcdc617556a29766a
Aryan Tiwari <at150@snu.edu.in>
2022-11-07 13:30:58 +0530

    variables in c++

---
+++ 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numbers = 5; // declaring and initialising one variable
+    int var1, var2, var3; // declaring multiple variables simultaneously
+    cout << "the entered number is " << numbers << endl;
+    numbers = 45;
+    cout << "the entered number is " << numbers << endl;
+    var1 = 13;
+    var2 = 14;
+    var3 = 15;
+    cout << "the value of var1 is: " << var1 << endl;
+    cout << "the value of var2 is: " << var2 << endl;
+    cout << "the value of var3 is: " << var3 << endl;
+    cout << "it is just a testing";
+}
