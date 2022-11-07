201d4a03fce32633bbfc5d5097309aadab8ccf02
Aryan Tiwari <at150@snu.edu.in>
2022-11-08 00:42:14 +0530

    type casting

--- data_type_modifiers.cpp
+++ data_type_modifiers.cpp
@@ -6,7 +6,13 @@ int main() {
   // Initialize variables
   int integer = 32768;
   short int Sinteger = 32768;
+  char var1 = 'A';
   // Display variables value
   cout << "integer = " << integer << endl;
   cout << "short_integer = " << Sinteger << endl;
+
+  // TYPE CASTING:
+  /* type casting is a way to convert the value of 
+  one data type to another data type. */
+  cout << int(var1);
 }
