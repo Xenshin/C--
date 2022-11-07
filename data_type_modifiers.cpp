763bfd101df8cba9a2bbc129c7334f9939c89a9a
Aryan Tiwari <at150@snu.edu.in>
2022-11-07 23:53:09 +0530

    value to int higher than the upper limit of int data type

---
+++ data_type_modifiers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //initialize variables
+    int integer = 2147483649; // this is the value greater than the upper limit of the int data type
+    //display variable value
+    cout << integer;
+}
