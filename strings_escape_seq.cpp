a1035d2d00a56d6709e5219e967e3290277c338d
Aryan Tiwari <at150@snu.edu.in>
2022-11-08 18:26:41 +0530

    arithmetic operations

--- strings_escape_seq.cpp
+++ strings_escape_seq.cpp
@@ -6,7 +6,15 @@ int main()
     //initialize a string variable
     string text1 = "hi ElectriX#5007";
     string text2 = "hello\nI\tam\tjohn";
+    float number;
+    char operand1 = 'n';
+    char operand2 = 'C';
     //displaying 
-    cout << text1;
-    cout << text2;    
+    cout << text1 << endl;
+    cout << text2 << endl;
+    cout << "please enter your number: " << endl;
+    cin >> number;
+    cout << "you have entered the number " << number << endl;
+    cout << "Division = " << operand1/operand2 << endl;
+    cout << "Modulus = " << operand1 % operand2 << endl;    
 }
