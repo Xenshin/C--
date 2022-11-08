54e6eb115a9397c264707ab92aef58eab6f63d84
Aryan Tiwari <at150@snu.edu.in>
2022-11-08 18:38:43 +0530

    if and else conditional statements

---
+++ 8if_else.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int money = 10;
+    // if condition
+    if (money >=20)
+    {
+        // if body
+        cout << "you can gift a watch" << endl;
+    }
+    else 
+    {
+        cout << "you can gift a pen" << endl;
+    }
+    return 0;
+}
