d1326d525864ba41ec02db02bacbb96535698e0c
Aryan Tiwari <at150@snu.edu.in>
2022-11-08 18:41:43 +0530

    nested else if

--- 8if_else.cpp
+++ 8if_else.cpp
@@ -3,16 +3,20 @@ using namespace std;
 
 int main()
 {
-    int money = 10;
+    int money = 4;
     // if condition
     if (money >=20)
     {
         // if body
         cout << "you can gift a watch" << endl;
     }
-    else 
+    else if (money >= 5)
     {
         cout << "you can gift a pen" << endl;
+    }
+    else 
+    {
+        cout << "you cant gift anything" << endl;
     }
     return 0;
 }
