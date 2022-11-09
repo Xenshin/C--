df34b465567f0ca8da622dbbe0065d7897f4b354
Aryan Tiwari <at150@snu.edu.in>
2022-11-09 12:03:20 +0530

    for loop

---
+++ 13_for_loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int icecream;
+    for (icecream = 5; icecream > 0; icecream --)
+    {
+        cout << "number of free icecream = " << icecream << endl;
+        cout << "Buy an icecream" << endl;
+    }
+    return 0;
+}
